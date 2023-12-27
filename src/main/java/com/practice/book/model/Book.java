8d1cc29c63999c227fab1d71858f99afd7777c0d
aishayusuff <aishatyusuffe@gmail.com>
2023-12-27 21:55:08 +0100

    adds getters/setters/applydiscount mthd to entity

---
+++ src/main/java/com/practice/book/model/Book.java
@@ -0,0 +1,67 @@
+package com.practice.book.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private double price;
+    private int quantityOfStock;
+
+    //Default constructor
+    public Book() {
+
+    }
+
+    //Parameterised construtor
+    public Book(String newTitle, String newAuthor, double newPrice, int newQuantityOfStock) {
+        this.title = newTitle;
+        this.author = newAuthor;
+        this.price = newPrice;
+        this.quantityOfStock = newQuantityOfStock; 
+
+    }
+
+    //Getters
+    public Long getID() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getQuantityOfStock() {
+        return this.quantityOfStock;
+    }
+
+    //Setter for modifying price state
+    protected void setPrice(double modifiedPrice) {
+        this.price = modifiedPrice;
+
+    }
+
+    protected void setQuantityOfStock(int modifiedQuantityOfStock) {
+        this.quantityOfStock = modifiedQuantityOfStock; 
+    }
+    
+    //Modifies the price in the new instance w/o mutating the existing price
+    public Book applyDiscount(double discountPercent) {
+        double discountedPrice = price - (price * discountPercent / 100);
+        return new Book(title, author, discountedPrice, quantityOfStock);
+    }
+
+}
