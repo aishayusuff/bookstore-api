4b5b24e98444346622ee43aa25a40c4dcb739eaa
aishayusuff <aishatyusuffe@gmail.com>
2023-12-27 22:05:54 +0100

    creates book repository

--- src/main/java/com/practice/book/repository/repository.java
+++ src/main/java/com/practice/book/repository/repository.java
@@ -1,5 +1,11 @@
 package com.practice.book.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.practice.book.model.Book;
+
 public class Repository {
-    
+    public interface BookRepository extends JpaRepository<Book, Long> {
+        
+    }
 }
--- src/main/java/com/practice/book/service/bookService.java
+++ src/main/java/com/practice/book/service/bookService.java
@@ -1,5 +1,5 @@
 package com.practice.book.service;
 
-public class bookService {
+public class BookService {
     
 }
