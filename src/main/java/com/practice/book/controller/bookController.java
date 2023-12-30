92c1e60ba5e8b42acf29b989087a3bb664c8bcb6
aishayusuff <aishatyusuffe@gmail.com>
2023-12-30 23:22:30 +0100

    modifies endpoint and adds ResponseEntity

--- src/main/java/com/practice/book/controller/bookController.java
+++ src/main/java/com/practice/book/controller/bookController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.practice.book.model.Book;
 import com.practice.book.model.BookDTO;
 import com.practice.book.service.BookService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@RequestMapping("/api/v1/books")
+@RequestMapping("/api/v1/bookerly")
 public class BookController {
     private final BookService bookService;
 
@@ -19,10 +22,10 @@ public class BookController {
     }
 
     @PostMapping("/create-books")
-    public Book createsBook(@RequestBody BookDTO newBook) {
+    public ResponseEntity<Book> createsBook(@RequestBody BookDTO newBook) {
         Book book = newBook.toEntity();
         Book createdBook = bookService.createNewBook(book);
-        return createdBook;
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdBook);
     }
 
 }
