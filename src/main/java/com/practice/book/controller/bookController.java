16934e381d45d003e0cdc4213ee89aaed02b9d69
aishayusuff <aishatyusuffe@gmail.com>
2023-12-31 23:58:45 +0100

    adds Delete mapping and a TODO for put mapping

--- src/main/java/com/practice/book/controller/bookController.java
+++ src/main/java/com/practice/book/controller/bookController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -50,4 +51,13 @@ public class BookController {
         return ResponseEntity.ok(BookDTO.fromEntity(book));
     }
 
+    //TODO:@PutMapping
+
+
+    //To delete a book
+    @DeleteMapping("/books/{id}")
+    public ResponseEntity<String> deleteBookById(@PathVariable Long id) {
+        bookService.deleteBookByID(id);
+        return ResponseEntity.ok("Book has been deleted successfully!");
+    }
 }
