c166feada1d8348ae77b5f8541f4dd2c048dfca1
aishayusuff <aishatyusuffe@gmail.com>
2023-12-31 23:47:27 +0100

    fixes DB bug and adds GetMapping to get book by id

--- src/main/java/com/practice/book/controller/bookController.java
+++ src/main/java/com/practice/book/controller/bookController.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -42,5 +43,11 @@ public class BookController {
         return ResponseEntity.ok(allBookDTOs);
     }
     
+    //To get one book
+    @GetMapping("/books/{id}")
+    public ResponseEntity<BookDTO> getBookById(@PathVariable Long id) {
+        Book book = bookService.getBookByID(id);
+        return ResponseEntity.ok(BookDTO.fromEntity(book));
+    }
 
 }
